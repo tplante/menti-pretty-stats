7a033ed561c122bb46b82cd930f3189459615857
Taylor Plante <taylorplante99@gmail.com>
2019-02-28 11:08:05 +0100

    cleaning, make <Content> component for Slam

---
+++ src/Content.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Box, Text } from "@mentimeter/ragnar-web";
+
+const Content = () => (
+  <Box alignItems="center" width="100%">
+    <Text fontSize={100} fontWeight="bold">
+      Pretty stats
+    </Text>
+    {/*
+     * Slam's React Code! (remove the curly braces around this comment)
+     */}
+  </Box>
+);
+
+export default Content;
