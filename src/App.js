bca7ee79ba55d087c3704a57d33127a362bb0bed
Taylor Plante <taylorplante99@gmail.com>
2019-02-28 10:04:36 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Trail, animated } from 'react-spring';
+import { Box, Absolute, BoxBorder, Image, Text } from '@mentimeter/ragnar-web';
+
+const items = [
+  'https://static.mentimeter.com/assets/awesome_individuals/ragnar-8d259309a742f0b22e31d9ba98cde5f42f9548be06acdb93b6124320acf2f337.jpg',
+  'https://static.mentimeter.com/assets/awesome_individuals/emma-k-279f7752569947e0c27dc16057b1254f53288ff4deb14b2dcff380b3392a1cf9.jpg',
+  'https://static.mentimeter.com/assets/awesome_individuals/victor-120d9cfa762a5f47b95b560588d85a36b365a0e952fa035b34e51a6840e22c82.jpg',
+  'https://static.mentimeter.com/assets/awesome_individuals/christofer-e299a73eed9fb41806eced72876d1802ffc5350a03b66facc8f194a7d76a05cd.jpg',
+];
 
 class App extends Component {
+  state = { coords: [0, 0] };
+
+  handleMouseMove = ({ pageX, pageY }) =>
+    this.setState({ coords: [pageX, pageY] });
+
+  transform = (x, y) => `translate3d(${x - 50}px, ${y - 50}px, 0)`;
+
+  componentDidMount = () =>
+    window.addEventListener('mousemove', this.handleMouseMove);
+
   render() {
+    const { coords } = this.state;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Box maxWidth="100vw">
+        <Box alignItems="center" width="100%">
+          <Text fontSize={100} fontWeight="bold">
+            RAGNAR
+          </Text>
+        </Box>
+        <Trail native items={items} to={{ coords }}>
+          {(src, i) => props => (
+            <animated.div
+              style={{
+                zIndex: items.length - i,
+                transform: props.coords.interpolate(this.transform),
+              }}
+            >
+              <Absolute>
+                <BoxBorder bg="yellow" size="140px" borderRadius={3}>
+                  <Image src={src} />
+                </BoxBorder>
+              </Absolute>
+            </animated.div>
+          )}
+        </Trail>
+      </Box>
     );
   }
 }
